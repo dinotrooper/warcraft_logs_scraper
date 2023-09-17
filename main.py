30e83f4d4671271478682ae0bcc7bc5af7fcbdfe
Alex Ward <dinotrooper@gmail.com>
2023-09-17 04:06:21 -0400

    getting encounter names now

--- main.py
+++ main.py
@@ -22,25 +22,33 @@ def get_encounter_ids() -> List[str]:
         id_list.append(tmp_id.strip())
     return id_list
 
-def get_encounter_name(encounter_id) -> str:
-    pass
-
+def get_encounter_name(wcl: ApiConnector, encounter_id) -> str:
+    log_dir = "./log/"
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+    last_response_filename = "last_response.txt"
+    response_json = wcl.generic_request(get_encounter_query(encounter_id))
+    if not is_response_good(response_json):
+        logging.error(f"response_json is bad: {response_json}")
+        return
+    with open(os.path.join(log_dir, last_response_filename), "w") as last_respone_file:
+        json.dump(response_json, last_respone_file)
+    try:
+        encounter_name = response_json["data"]["worldData"]["encounter"]["name"]
+    except:
+        logging.error("encounter name not in response json")
+        logging.debug("response_json = %s", response_json)
+        return
+    return encounter_name
+    
 
 def get_encounter_query(encounter_id) -> str:
-    name = name.strip()
+    encounter_id = encounter_id.strip()
     return f"""
     query {{
-        characterData {{
-            character(
-                name: "{name}"
-                serverSlug: "Faerlina"
-                serverRegion: "US"
-            ) {{
-                id
+        worldData {{
+            encounter(id: {encounter_id}) {{
                 name
-                encounterRankings(
-                    encounterID: {encounter_id}
-                )
             }}
         }}
     }}
@@ -83,15 +91,13 @@ def main():
     with open(".keyfile") as f:
         keys = json.load(f)
     wcl = ApiConnector(keys["client_id"], keys["client_secret"], logging)
-    for name in get_names():
-        for encounter_id in get_encounter_ids():
-            with open(output_fp, "w") as output_file:
-                response = get_median_perf(wcl, name, encounter_id)
-                logging.debug("response = %s", response)
-                if response == None:
-                    continue
-                encounter_name, median_perf = response
-                output_file.write(f"{name} - median perf: {median_perf} on {encounter_name}\n")
+    with open(output_fp, "w") as output_file:
+        for name in get_names():
+            # for encounter_id in get_encounter_ids():
+            encounter_id = get_encounter_ids()[0]
+            encounter_name = get_encounter_name(wcl, encounter_id)
+            median_perf = get_median_perf(wcl, name, encounter_id)
+            output_file.write(f"{name} - median perf: {median_perf} on {encounter_name}\n")
 
 def get_median_perf(wcl: ApiConnector, name: str, encounter_id: str):
     response_json = wcl.generic_request(get_best_perf_avg_query(name, encounter_id))
@@ -99,18 +105,13 @@ def get_median_perf(wcl: ApiConnector, name: str, encounter_id: str):
         logging.error(f"response_json is bad: {response_json}")
         return
     try:
-        encounter_name = response_json["data"]["characterData"]["character"]["encounterRankings"]["name"]
-    except:
-        encounter_name = None
-        logging.error("response_json is missing encounter_name")
-        logging.debug("response_json = %s", response_json)
-    try:
         median_perf = response_json["data"]["characterData"]["character"]["encounterRankings"]["medianPerformance"]
     except:
         logging.error("response_json is missing median_perf")
         logging.debug("response_json = %s", response_json)
-        median_perf = None
-    return encounter_name, median_perf
+        return None
+    median_perf = round(float(median_perf), 2)
+    return median_perf
 
 def is_response_good(response):
     return not any([
